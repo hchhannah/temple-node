57a899fd19dadd7ca9ad451fad59f151cce1e389
hchhannah <chize9863@gmail.com>
2023-07-20 13:25:25 +0800

    ?寫法

--- routes/products-api.js
+++ routes/products-api.js
@@ -9,15 +9,10 @@ const multipartParser = upload.none();
 router.post('/', async (req, res) => {
     // 從前端傳來的資料
     const requestData = req.body.requestData; 
-    
-    const [cid] = await db.query(
-          `SELECT \`cid\` FROM \`categories\` WHERE \`category_name\` = '${requestData[0].id}';`
-    )
-    
-    const [data] = await db.query(
-          `SELECT * FROM \`products\` WHERE \`cid\` = ${cid[0].cid} ORDER BY \`purchase_num\` DESC LIMIT 10;`
-    )
-    
+    const sql1 = `SELECT \`cid\` FROM \`categories\` WHERE \`category_name\` = ?;`
+    const [cid] = await db.query( sql1, [requestData[0].id])
+    const sql2 =  `SELECT * FROM \`products\` WHERE \`cid\` = ? ORDER BY \`purchase_num\` DESC LIMIT 10;`
+    const [data] = await db.query(sql2,[cid[0].cid])    
     res.json(data)
   });
 
@@ -28,23 +23,16 @@ router.get('/:category',async (req,res)=>{
     
     // 全部
     if(category==='all'){
-
-        const [data] = await db.query(
-            `SELECT * FROM products `
-            )
-            res.json(data)
+        const [data] = await db.query(`SELECT * FROM products `)
+        res.json(data)
     
     // 依照類別去抓
     }else{
-
-        const [cid] = await db.query(
-            `SELECT \`cid\` FROM \`categories\` WHERE \`category_name\` = '${category}';`
-        )
-                
-        const [data] = await db.query(
-            `SELECT * FROM \`products\` WHERE \`cid\` = ${cid[0].cid} `
-            )
-            res.json(data)
+        const sql1 = `SELECT \`cid\` FROM \`categories\` WHERE \`category_name\` = ?;`
+        const [cid] = await db.query(sql1,[category])
+        const sql2 =   `SELECT * FROM \`products\` WHERE \`cid\` = ? `
+        const [data] = await db.query( sql2,[cid[0].cid])
+        res.json(data)
         }
 
 })
@@ -53,10 +41,8 @@ router.get('/:category',async (req,res)=>{
 // 動態路由來抓資料
 router.get('/:category/:pid',async (req,res)=>{
     const pid = req.params.pid;
-         
-    const [data] = await db.query(
-        `SELECT p.* , c.\`category_name\` FROM \`products\` p JOIN \`categories\` c ON p.\`cid\` = c.\`cid\` WHERE p.\`pid\`=${pid};`
-        )
+    const sql = `SELECT \`p\`.* , \`c\`.\`category_name\` FROM \`products\` p JOIN \`categories\` c ON p.\`cid\` = c.cid WHERE p.pid=?;`
+    const [data] = await db.query(sql , [pid])
     res.json(data)
 })
 
