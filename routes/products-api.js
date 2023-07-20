9ae95d707a032b07189a5965addf480cde56ca28
hchhannah <chize9863@gmail.com>
2023-07-20 13:05:30 +0800

    join

--- routes/products-api.js
+++ routes/products-api.js
@@ -25,7 +25,7 @@ router.post('/', async (req, res) => {
 // 動態路由來抓資料
 router.get('/:category',async (req,res)=>{
     const category = req.params.category;
-
+    
     // 全部
     if(category==='all'){
 
@@ -36,16 +36,28 @@ router.get('/:category',async (req,res)=>{
     
     // 依照類別去抓
     }else{
+
         const [cid] = await db.query(
             `SELECT \`cid\` FROM \`categories\` WHERE \`category_name\` = '${category}';`
         )
                 
         const [data] = await db.query(
-            `SELECT p.* , c.category_name FROM \`products\` p JOIN \`categories\` c ON p.cid = c.cid WHERE p.cid = '${cid[0].cid}';`
+            `SELECT * FROM \`products\` WHERE \`cid\` = ${cid[0].cid} `
             )
             res.json(data)
         }
 
+})
+
+
+// 動態路由來抓資料
+router.get('/:category/:pid',async (req,res)=>{
+    const pid = req.params.pid;
+         
+    const [data] = await db.query(
+        `SELECT p.* , c.\`category_name\` FROM \`products\` p JOIN \`categories\` c ON p.\`cid\` = c.\`cid\` WHERE p.\`pid\`=${pid};`
+        )
+    res.json(data)
 })
 
 module.exports = router;
