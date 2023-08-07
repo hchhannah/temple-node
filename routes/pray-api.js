1b59dc1c466208ef8cb6224cdff3d7374655cc39
Pluto1990 <boomipon@gmail.com>
2023-08-07 11:14:32 +0800

    pray

--- routes/pray-api.js
+++ routes/pray-api.js
@@ -47,16 +47,14 @@ const [result] = await db.query(sql,[
 router.post('/loveB-2', async (req,res)=>{
     console.log(req.body.requestData );
     const Member_ID = '12'
-    const All_Light = 'aaa'
 //  const {Tower_ID, All_Light, LocationX,LocationY,Datetime} = req.body.requestData   
 const sql = "INSERT INTO `Love_Light`" +
-"(`Member_ID`,`Tower_ID`, `All_Light`, `LocationX`,`LocationY`,`Datetime`)" +
-" VALUES ( ?, ?, ?, ?, ?, NOW())";
+"(`Member_ID`,`Tower_ID`, `LocationX`,`LocationY`,`Datetime`)" +
+" VALUES ( ?, ?, ?, ?, NOW())";
 console.log('r:',Member_ID);
 const [result] = await db.query(sql,[
     Member_ID,
     req.body.Tower_ID,
-    All_Light,
     req.body.LocationX,
     req.body.LocationY,
 ])
@@ -64,6 +62,39 @@ const [result] = await db.query(sql,[
         result,
         postData: req.body
     })
+});
+
+router.get("/loveB-2", async (req, res) =>{
+    let output = {
+        redirect : '',
+        totalRows :0, 
+        perPage :8, 
+        totalPages :0, 
+        page :1 , 
+        rows :[]
+    };
+    const perPage = 8; //每一頁幾筆資料
+    let keyword = req.query.keyword || '';
+    let page = req.query.page ? parseInt(req.query.page) : 1;
+    if(!page || page<1) {
+        output.redirect = req.baseUrl;
+        return res.json(output);
+    };
+    const t_sql = `SELECT COUNT(1) totalRows FROM Love_Light`;
+    const [[{totalRows}]] = await db.query(t_sql);
+    let totalPages = 0;
+    let rows = [];
+    if(totalRows){
+        totalPages = Math.ceil(totalRows/perPage);
+        if(page > totalPages) {
+            output.redirect = req.baseUrl + '?page=' + totalPages;
+            return res.json(output);
+        };
+        const sql = ` SELECT * FROM Love_Light LIMIT ${perPage * (page-1)}, ${perPage} `;
+        [rows] = await db.query(sql);
+    }
+    output = {...output, totalRows, perPage, totalPages, page, rows};
+    return res.json(output);
 });
 // 測試求籤
 router.get("/", async (req, res) => {
