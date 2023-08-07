e7e76837abe93a1a7e76f2defbe30443d07a6b46
hchhannah <chize9863@gmail.com>
2023-08-07 14:18:40 +0800

    0807

--- routes/worship-api.js
+++ routes/worship-api.js
@@ -4,10 +4,16 @@ const router = express.Router();
 const db = require(__dirname+'/../modules/mysql2');
 const upload = require(__dirname+'/../modules/img-upload.js');
 const multipartParser = upload.none();
+
 router.get('/', async (req, res) => {
     const sql_mazu = `SELECT * FROM \`worship\` WHERE \`cid\`=36`
     const [mazu_data] = await db.query(sql_mazu)
-    res.json(mazu_data)
+    const sql_love = `SELECT * FROM \`worship\` WHERE \`cid\`=37`
+    const [love_data] = await db.query(sql_love)
+    const sql_study = `SELECT * FROM \`worship\` WHERE \`cid\`=38`
+    const [study_data] = await db.query(sql_study)
+    const data =[mazu_data, love_data, study_data] 
+    res.json(data)
 })
 
 module.exports = router;
