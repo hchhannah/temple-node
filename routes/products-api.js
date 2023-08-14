2dbff4d3fb20b204791259fbea816f218bf2e4f7
hchhannah <chize9863@gmail.com>
2023-08-14 13:53:29 +0800

    0814

--- routes/products-api.js
+++ routes/products-api.js
@@ -352,7 +352,6 @@ router.post('/:category',async (req,res)=>{
     const {page, perPage, sort, orderBy, keyword} = req.body.requestData;
     let totalPages = 0
     let where = 'WHERE 1'
-    // console.log(keyword);
     
     // 依照類別去改變WHERE條件
     if(category!=='all'){
